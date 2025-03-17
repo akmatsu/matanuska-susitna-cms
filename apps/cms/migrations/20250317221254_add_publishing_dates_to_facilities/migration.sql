fbf667c0976ae13ddb1b4ee0e6b6bc97ee3e2854
f3ve <free.eve@pm.me>
2025-03-17 14:15:53 -0800

    feat: facilities now searchable

---
+++ apps/cms/migrations/20250317221254_add_publishing_dates_to_facilities/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Facility" ADD COLUMN     "publishAt" TIMESTAMP(3),
+ADD COLUMN     "reviewDate" TIMESTAMP(3),
+ADD COLUMN     "unpublishAt" TIMESTAMP(3);
--- apps/cms/src/app/models/Facility.ts
+++ apps/cms/src/app/models/Facility.ts
@@ -5,6 +5,7 @@ import {
   contacts,
   liveUrl,
   owner,
+  publishable,
   services,
   slug,
   tags,
@@ -14,8 +15,24 @@ import {
 } from '../fieldUtils';
 import { relationship } from '@keystone-6/core/fields';
 import { customText } from '../../components/customFields/Markdown';
+import { TYPESENSE_CLIENT, TYPESENSE_COLLECTIONS } from '../../utils/typesense';
 
 const listPlural = 'facilities';
+
+export function facilityToSearchableObj(item: any) {
+  return {
+    id: item.id,
+    title: item.title,
+    description: item.description,
+    body: item.body,
+    slug: item.slug,
+    published_at: item.publishAt
+      ? Math.floor(new Date(item.publishAt).getTime() / 1000)
+      : undefined,
+    tags: item.tags?.map((tag: { name: string }) => tag.name || ''),
+    type: 'facility',
+  };
+}
 
 export const Facility: ListConfig<any> = list({
   access: {
@@ -25,6 +42,7 @@ export const Facility: ListConfig<any> = list({
   fields: {
     heroImage: blueHarvestImage(),
     ...titleAndDescription(),
+    ...publishable,
     liveUrl: liveUrl(listPlural),
     slug,
     owner,
@@ -51,5 +69,35 @@ export const Facility: ListConfig<any> = list({
     park: relationship({ ref: 'Park.facilities', many: false }),
     hours: relationship({ ref: 'OperatingHour', many: true }),
     ...timestamps,
+  },
+
+  hooks: {
+    beforeOperation: {
+      async delete({ item }) {
+        try {
+          TYPESENSE_CLIENT.collections(TYPESENSE_COLLECTIONS.PAGES)
+            .documents(item.id.toString())
+            .delete();
+        } catch (e) {
+          console.error('Error deleting from Typesense', e);
+        }
+      },
+    },
+    async afterOperation({ operation, item, context }) {
+      if (operation === 'update' || operation === 'create') {
+        const trail = await context.query.Facility.findOne({
+          where: { id: item.id.toString() },
+          query:
+            'id title description body slug liveUrl publishAt tags {name} owner {name} services {title}',
+        });
+        try {
+          await TYPESENSE_CLIENT.collections(TYPESENSE_COLLECTIONS.PAGES)
+            .documents()
+            .upsert(facilityToSearchableObj(item));
+        } catch (e) {
+          console.error('Error upserting to Typesense', e);
+        }
+      }
+    },
   },
 });
