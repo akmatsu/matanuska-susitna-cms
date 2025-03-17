2b4d01bbbed5222d7be5115cf9a1d01dd118ea6e
f3ve <free.eve@pm.me>
2025-03-17 13:41:05 -0800

    feat: parks now searchable

--- apps/cms/src/app/models/Park.ts
+++ apps/cms/src/app/models/Park.ts
@@ -5,6 +5,7 @@ import {
   contacts,
   liveUrl,
   owner,
+  publishable,
   services,
   slug,
   tags,
@@ -14,8 +15,31 @@ import {
 } from '../fieldUtils';
 import { relationship } from '@keystone-6/core/fields';
 import { customText } from '../../components/customFields/Markdown';
+import { TYPESENSE_CLIENT, TYPESENSE_COLLECTIONS } from '../../utils/typesense';
 
 const listPlural = 'parks';
+
+export function parkToSearchableObj(item: any) {
+  return {
+    id: item.id,
+    title: item.title,
+    description: item.description,
+    body: item.body,
+    slug: item.slug,
+    published_at: item.publishAt
+      ? Math.floor(new Date(item.publishAt).getTime() / 1000)
+      : undefined,
+    tags: item.tags.map((tag: { name: string }) => tag.name || ''),
+    services: item.services.map(
+      (service: { title: string }) => service.title || '',
+    ),
+    trails: item.trails.map((trail: { title: string }) => trail.title || ''),
+    facilities: item.facilities.map(
+      (facility: { title: string }) => facility.title || '',
+    ),
+    type: 'park',
+  };
+}
 
 export const Park: ListConfig<any> = list({
   access: {
@@ -25,6 +49,7 @@ export const Park: ListConfig<any> = list({
   fields: {
     heroImage: blueHarvestImage(),
     ...titleAndDescription(),
+    ...publishable,
     liveUrl: liveUrl(listPlural),
     slug,
     owner,
@@ -65,5 +90,36 @@ export const Park: ListConfig<any> = list({
       },
     }),
     ...timestamps,
+  },
+  hooks: {
+    beforeOperation: {
+      async delete({ item }) {
+        try {
+          TYPESENSE_CLIENT.collections(TYPESENSE_COLLECTIONS.PAGES)
+            .documents(item.id.toString())
+            .delete();
+        } catch (e) {
+          console.error('Error deleting Typesense document', e);
+        }
+      },
+    },
+    async afterOperation({ operation, context, item }) {
+      if (operation === 'update' || operation === 'create') {
+        try {
+          console.log('ran UPDATE!');
+          const park = await context.query.Park.findOne({
+            where: { id: item.id.toString() },
+            query:
+              'id title description body slug liveUrl publishAt owner {name} tags {name} services {title} trails {title} facilities {title}',
+          });
+          const searchableObj = parkToSearchableObj(park);
+          await TYPESENSE_CLIENT.collections(TYPESENSE_COLLECTIONS.PAGES)
+            .documents()
+            .upsert(searchableObj);
+        } catch (e: any) {
+          console.error('Error updating Typesense document', e);
+        }
+      }
+    },
   },
 });
