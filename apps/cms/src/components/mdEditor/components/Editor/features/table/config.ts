4ff8caf938e9202f83af11cd60fd5733875705f1
f3ve <free.eve@pm.me>
2025-04-07 15:25:32 -0800

    feat: made delete row and column buttons more explicit in tables

--- apps/cms/src/components/mdEditor/components/Editor/features/table/config.ts
+++ apps/cms/src/components/mdEditor/components/Editor/features/table/config.ts
@@ -9,6 +9,28 @@ export function configureTableFeature(editor: Editor) {
     .config((ctx) => {
       ctx.update(tableBlockConfig.key, (defaultConfig) => ({
         ...defaultConfig,
+        renderButton(renderType) {
+          switch (renderType) {
+            case 'add_row':
+              return '+';
+            case 'add_col':
+              return '+';
+            case 'delete_row':
+              return 'Delete Row';
+            case 'delete_col':
+              return 'Delete Column';
+            case 'align_col_left':
+              return 'Left';
+            case 'align_col_center':
+              return 'Center';
+            case 'align_col_right':
+              return 'Right';
+            case 'col_drag_handle':
+              return '=';
+            case 'row_drag_handle':
+              return '=';
+          }
+        },
       }));
     })
     .use(tableBlock);
