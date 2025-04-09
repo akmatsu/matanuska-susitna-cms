0ee615cbf19f247bd365af4ba26b3f29e490ebcf
f3ve <free.eve@pm.me>
2025-04-09 12:07:19 -0800

    feat: using the basePage util function in facility

--- apps/cms/src/app/models/Facility.ts
+++ apps/cms/src/app/models/Facility.ts
@@ -1,38 +1,14 @@
 import { list, ListConfig } from '@keystone-6/core';
 import { generalItemAccess, generalOperationAccess } from '../access';
-import { blueHarvestImage } from '../../components/customFields/blueHarvestImage';
 import {
-  contacts,
-  liveUrl,
-  owner,
-  publishable,
+  basePage,
   services,
-  slug,
-  tags,
-  timestamps,
-  titleAndDescription,
-  userGroups,
+  typesenseDelete,
+  typesenseUpsert,
 } from '../fieldUtils';
 import { relationship } from '@keystone-6/core/fields';
-import { customText } from '../../components/customFields/Markdown';
-import { TYPESENSE_CLIENT, TYPESENSE_COLLECTIONS } from '../../utils/typesense';
 
 const listPlural = 'facilities';
-
-export function facilityToSearchableObj(item: any) {
-  return {
-    id: item.id,
-    title: item.title,
-    description: item.description,
-    body: item.body,
-    slug: item.slug,
-    published_at: item.publishAt
-      ? Math.floor(new Date(item.publishAt).getTime() / 1000)
-      : undefined,
-    tags: item.tags?.map((tag: { name: string }) => tag.name || ''),
-    type: 'facility',
-  };
-}
 
 export const Facility: ListConfig<any> = list({
   access: {
@@ -40,77 +16,23 @@ export const Facility: ListConfig<any> = list({
     item: generalItemAccess('Facility'),
   },
   fields: {
-    heroImage: blueHarvestImage(),
-    ...titleAndDescription(),
-    ...publishable,
-    liveUrl: liveUrl(listPlural),
-    slug,
-    owner,
-    body: customText(),
-    tags: tags(listPlural),
-    userGroups: userGroups(listPlural),
-    address: relationship({
-      ref: 'Location',
-      many: false,
-      ui: {
-        displayMode: 'cards',
-        cardFields: ['title', 'lineOne', 'lineTwo', 'city', 'state', 'zip'],
-        inlineCreate: {
-          fields: ['title', 'lineOne', 'lineTwo', 'city', 'state', 'zip'],
-        },
-        inlineConnect: true,
-        itemView: {
-          fieldPosition: 'sidebar',
-        },
-      },
+    ...basePage(listPlural, {
+      address: true,
     }),
-    contacts: contacts(listPlural),
     services: services(listPlural),
     park: relationship({ ref: 'Park.facilities', many: false }),
-    hours: relationship({
-      ref: 'OperatingHour',
-      many: true,
-      ui: {
-        displayMode: 'cards',
-        cardFields: ['day', 'open', 'close'],
-        inlineCreate: {
-          fields: ['day', 'open', 'close'],
-        },
-        inlineEdit: {
-          fields: ['day', 'open', 'close'],
-        },
-      },
-    }),
-    ...timestamps,
   },
 
   hooks: {
-    beforeOperation: {
-      async delete({ item }) {
-        try {
-          TYPESENSE_CLIENT.collections(TYPESENSE_COLLECTIONS.PAGES)
-            .documents(item.id.toString())
-            .delete();
-        } catch (e) {
-          console.error('Error deleting from Typesense', e);
-        }
-      },
+    beforeOperation(args) {
+      typesenseDelete(args);
     },
-    async afterOperation({ operation, item, context }) {
-      if (operation === 'update' || operation === 'create') {
-        const trail = await context.query.Facility.findOne({
-          where: { id: item.id.toString() },
-          query:
-            'id title description body slug liveUrl publishAt tags {name} owner {name} services {title}',
-        });
-        try {
-          await TYPESENSE_CLIENT.collections(TYPESENSE_COLLECTIONS.PAGES)
-            .documents()
-            .upsert(facilityToSearchableObj(item));
-        } catch (e) {
-          console.error('Error upserting to Typesense', e);
-        }
-      }
+    async afterOperation(args) {
+      typesenseUpsert(
+        'facility',
+        'id title description body slug liveUrl publishAt tags {name} owner {name} services {title}',
+        args,
+      );
     },
   },
 });
