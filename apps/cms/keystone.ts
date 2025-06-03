7cf284a25e1b00c32161aeb166d7ba5fc3918afe
f3ve <free.eve@pm.me>
2025-06-03 13:27:41 -0800

    feat: created queries to dynamically grab data

--- apps/cms/keystone.ts
+++ apps/cms/keystone.ts
@@ -7,6 +7,10 @@ import { type Session } from './src/session';
 import { nextAuthSessionStrategy } from './src/session';
 import { routes } from './src/routes/baseRoutes';
 import { connectRedis, getPublishQueueEvents } from './src/queues/redis';
+import { mergeSchemas } from '@graphql-tools/schema';
+import { KeystoneContext } from '@keystone-6/core/types';
+import { graphqlExtendTypeDefs } from './src/utils/graphqlHelpers';
+import { logger } from './src/configs/logger';
 
 export default config<TypeInfo<Session>>({
   // https://keystonejs.com/docs/config/config#db
@@ -74,6 +78,210 @@ export default config<TypeInfo<Session>>({
           to: '/api/auth/signin',
         };
       }
+    },
+  },
+
+  graphql: {
+    extendGraphqlSchema(schema) {
+      return mergeSchemas({
+        schemas: [schema],
+        typeDefs: graphqlExtendTypeDefs,
+        resolvers: {
+          InternalLinkSearch: {
+            __resolveType(value: any) {
+              if ('primaryActionId' in value) return 'Service';
+              if ('highlightThreeId' in value) return 'HomePage';
+              if ('linkToAgendasId' in value) return 'Board';
+              if ('vacancyReportId' in value) return 'BoardPage';
+              if ('effectiveDate' in value) return 'PublicNotice';
+              if ('parentId' in value) return 'OrgUnit';
+              if ('memberName' in value) return 'OrgUnit';
+              if ('url' in value) return 'Url';
+              console.log(value);
+            },
+          },
+          Query: {
+            getInternalLink: async (
+              _,
+              { id, type }: { id: string; type: string },
+              context: KeystoneContext<TypeInfo<Session>>,
+            ) => {
+              switch (type) {
+                case 'Service':
+                  return context.db.Service.findOne({ where: { id } });
+                case 'Facility':
+                  return context.db.Facility.findOne({ where: { id } });
+                case 'HomePage':
+                  return context.db.HomePage.findOne({ where: { id } });
+                case 'Board':
+                  return context.db.Board.findOne({ where: { id } });
+                case 'BoardPage':
+                  return context.db.BoardPage.findOne({ where: { id } });
+                case 'PublicNotice':
+                  return context.db.PublicNotice.findOne({ where: { id } });
+                case 'OrgUnit':
+                  return context.db.OrgUnit.findOne({ where: { id } });
+                case 'AssemblyDistrict':
+                  return context.db.AssemblyDistrict.findOne({ where: { id } });
+                case 'Community':
+                  return context.db.Community.findOne({ where: { id } });
+                case 'Park':
+                  return context.db.Park.findOne({ where: { id } });
+                case 'Trail':
+                  return context.db.Trail.findOne({ where: { id } });
+                case 'Url':
+                  return context.db.Url.findOne({ where: { id } });
+                default:
+                  throw new Error(`Unknown type: ${type}`);
+              }
+            },
+            internalSearch: async (
+              info: any,
+              { query }: { query?: string },
+              context: KeystoneContext<TypeInfo<Session>>,
+            ) => {
+              const [
+                services,
+                facilities,
+                homePages,
+                boards,
+                boardPages,
+                publicNotices,
+                orgUnits,
+                assemblyDistricts,
+                communities,
+                parks,
+                trails,
+                urls,
+              ] = await Promise.all([
+                context.db.Service.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.Facility.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.HomePage.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.Board.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.BoardPage.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.PublicNotice.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.OrgUnit.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.AssemblyDistrict.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.Community.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.Park.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.Trail.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { body: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+                context.db.Url.findMany({
+                  where: {
+                    OR: [
+                      { title: { contains: query, mode: 'insensitive' } },
+                      { description: { contains: query, mode: 'insensitive' } },
+                      { url: { contains: query, mode: 'insensitive' } },
+                    ],
+                  },
+                }),
+              ]);
+
+              // 5. Combine all arrays into one heterogeneous array
+              return [
+                ...services,
+                ...facilities,
+                ...homePages,
+                ...boards,
+                ...boardPages,
+                ...publicNotices,
+                ...orgUnits,
+                ...assemblyDistricts,
+                ...communities,
+                ...parks,
+                ...trails,
+                ...urls,
+              ];
+            },
+          },
+        },
+      });
     },
   },
 });
