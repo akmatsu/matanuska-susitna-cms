4f450e5f83cc66e470e106d23cb08107861c71d5
f3ve <free.eve@pm.me>
2026-07-29 11:05:42 -0800

    fix: fixed migrations

---
+++ apps/cms/migrations/20260729190451_muffins/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "PublicNotice" ALTER COLUMN "type" SET DEFAULT 'none';
+
+-- AlterTable
+ALTER TABLE "PublicNoticeDraft" ALTER COLUMN "type" SET DEFAULT 'none';
+
+-- AlterTable
+ALTER TABLE "PublicNoticeVersion" ALTER COLUMN "type" SET DEFAULT 'none';
